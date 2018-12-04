bfec0e1e06fa5ca50c011dc361ec0c55c8caadb3
Mionsoo <pooy3460@gmail.com>
2018-12-04 23:26:10 +0900

    need supplement saveCrawlingDataToDB func

--- minwonCrawler.py
+++ minwonCrawler.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup as bs
-from bs4 import SoupStrainer
 import urllib.request as ul
 import requests
 import re
@@ -14,7 +13,8 @@ faq_url= "https://www.wetax.go.kr/main/?cmd=LPTIAD0R1&faqDiv=&faqField=&faqKeywo
 navigate_url = root_url + '/main/?cmd=LPTIIA1R1'
 
 def saveCrawlingDataToDB():
-    soup = get_soup(navigate_url)
+    # ToDo: This function need supplement
+    soup = getSoup(navigate_url)
     category_list = soup.find_all("div",{"class":"council"})
 
     i = 0
@@ -22,17 +22,22 @@ def saveCrawlingDataToDB():
     categories_list = []
     contents_list = []
 
+    '''
+    for _category in category_list:
+        name = _category.find("strong").get_text()
+        categ = [i.get_text() for i in _category.find_all("dt")]
+        categ_values = [' '.join(i.get_text().split()) for i in _category.find_all("dd")]
+
+        nameList.append(name)
+    '''
 
     for category_infos in category_list:
-
         category_data = category_infos.find(["dl"])
 
         nameList.append(category_infos.find("strong").get_text())
         categories = category_data.find_all("dt")
         category_contents = category_data.find_all("dd")
         categories2 = [value.get_text() for value in categories if value.get_text() not in categories_list if value.get_text() != "세율"]
-
-        # asd = {categories_list.index(value):category_contents[categories_list.index(value)] for value in categories if categories_list[value.get_text()]}
 
         categories_list = categories_list + categories2
         asd = [(categories_list.index(value.get_text()), category_contents[idx].get_text()) for idx,value in enumerate(categories) if value.get_text() in categories_list]
@@ -44,7 +49,7 @@ def saveCrawlingDataToDB():
 
     df = DataFrame(columns=column)
     for idx,val in enumerate(contents_list):
-        contents = [str("Null")] * (len(categories_list) + 2)
+        contents = [""] * (len(categories_list) + 2)
         contents[0] = str(idx+1)
         contents[1] = nameList[idx]
         for v in val:
@@ -56,7 +61,8 @@ def saveCrawlingDataToDB():
     conn = sqlite3.connect("minwon.db")
     df.to_sql("minwon_info",conn,if_exists="append",index=False)
 
-def get_soup(url):
+
+def getSoup(url):
     """
     get html response at url and make soup
     :param url: type (string), webpage url
@@ -64,15 +70,16 @@ def get_soup(url):
     """
     return bs(requests.get(url).text,'lxml')
 
-def get_faqCategory(root_url):
+
+def getFaqCategory(root_url):
     """
     get faqCategory id and content at url then make Dictionary set
-    ex ) 01 : 전자신고 ...
+    ex ) 01 : 전자신고 , 02 : 전자 납부 ...
     :param root_page: type (string) , webpage url
     :return: Dictionary { id : content, ... }
     """
     pageDict = {}
-    soup = get_soup(root_url+"/main/?cmd=LPTIAD0R1")
+    soup = getSoup(root_url+"/main/?cmd=LPTIAD0R1")
     pagelist = soup.find("div", {"class": "list_search"}).find_all("option")
 
     for i in pagelist:
@@ -87,15 +94,15 @@ def get_faqCategory(root_url):
 
 def crawling_AnswerByQuestion(question):
     '''
-    make searching func at Faq using question
-    get first row answer by using user question search
-    :param question: type (string) , user input string
-    :return: answer string
+    searching and making answer function at Faq with question
+    get FAQ first row answer using user question
+    :param question: user input string, type: String
+    :return: answer type: String
     '''
     #ToDo: need to speed up more
     # search by question
     crawler_startTimevect = time.time()
-    soup = get_soup(faq_url + ul.quote(question, encoding='euc-kr'))
+    soup = getSoup(faq_url + ul.quote(question, encoding='euc-kr'))
     faq_page = soup.find("ul", {"class": "faq"})
 
     try:
@@ -104,9 +111,11 @@ def crawling_AnswerByQuestion(question):
     except AttributeError:
         pass
 
-    answerUrl = soup.find("ul", {"class": "faq"}).find("a")['href']
-    soup = get_soup(root_url + answerUrl)
+    # get first row url parameter
+    answerUrlParam = soup.find("ul", {"class": "faq"}).find("a")['href']
+    soup = getSoup(root_url + answerUrlParam)
 
+    # get first row's question and answer and return
     faq_question = soup.find("dl", {"class": "w_view"}).find("strong").get_text()
     answer = soup.find("dl", {"class": "w_view"}).find("pre").get_text()
     print("Finished time: %0.2f Minutes" % ((time.time() - crawler_startTimevect) / 60))
