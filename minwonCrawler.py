d888df62272976efba229341d10f024e247b808b
Mionsoo <pooy3460@gmail.com>
2018-12-05 23:07:04 +0900

    181205 supplement saveCrawlingDataToDB()

--- minwonCrawler.py
+++ minwonCrawler.py
@@ -4,7 +4,8 @@ import requests
 import re
 import time
 import sqlite3
-from pandas import DataFrame,Series
+import string
+from pandas import DataFrame
 
 
 root_url = 'https://www.wetax.go.kr'
@@ -12,52 +13,52 @@ faq_each_page = 'https://www.wetax.go.kr/main/?cmd=LPTIAD0R1&faqDiv=@FAQPAGE@'
 faq_url= "https://www.wetax.go.kr/main/?cmd=LPTIAD0R1&faqDiv=&faqField=&faqKeyword="
 navigate_url = root_url + '/main/?cmd=LPTIIA1R1'
 
+
 def saveCrawlingDataToDB():
-    # ToDo: This function need supplement
+    # ToDo: This function need clean up
     soup = getSoup(navigate_url)
     category_list = soup.find_all("div",{"class":"council"})
 
-    i = 0
     nameList = []
     categories_list = []
     contents_list = []
+    columns = []
+    imgDict = {"자동차세": ("세율","car.png"), "지방교육세": ("납세의무자, 과세표준 및 세율","education.png"), "재산세": ("세율","jaesan.png"), "등록면허세(면허)": ("세율","myunher.png"), "지역자원시설세": ("과세표준과 세율","sisul.png")}
 
-    '''
+    # column count=12, # of hometax = 12
+    df = DataFrame(index=list(range(12)), columns=list(string.ascii_uppercase[:12]))
+
+    # make name, category, value for each categories then append to lists
     for _category in category_list:
         name = _category.find("strong").get_text()
-        categ = [i.get_text() for i in _category.find_all("dt")]
-        categ_values = [' '.join(i.get_text().split()) for i in _category.find_all("dd")]
+        categ = [i.get_text() for i in _category.find_all("dt")] + ["정보"]
+        info = [' '.join(i.next.split()) for i in _category.find_all("div")]
+        categ_values = [' '.join(i.get_text().split()) for i in _category.find_all("dd") if not i.find("div",{"class":"w_list"})] + info
+        for i in categ:
+            columns.append(i)
 
         nameList.append(name)
-    '''
+        categories_list.append(categ)
+        contents_list.append(zip(categ, categ_values))
 
-    for category_infos in category_list:
-        category_data = category_infos.find(["dl"])
+    # remove duplication columns
+    columns = ["name"] + list(set(columns))
+    # input columns to dataframe
+    df.columns = columns
 
-        nameList.append(category_infos.find("strong").get_text())
-        categories = category_data.find_all("dt")
-        category_contents = category_data.find_all("dd")
-        categories2 = [value.get_text() for value in categories if value.get_text() not in categories_list if value.get_text() != "세율"]
+    # input all values to dataframe
+    for idx,value in enumerate(contents_list):
+        df.ix[idx]["name"] = nameList[idx]
+        for i,_v in enumerate(list(value)):
+            k,v = _v
+            df.ix[idx][categories_list[idx][i]] = v
 
-        categories_list = categories_list + categories2
-        asd = [(categories_list.index(value.get_text()), category_contents[idx].get_text()) for idx,value in enumerate(categories) if value.get_text() in categories_list]
-        contents_list = contents_list + [asd]
+    # input imgPath to each categories
+    for i in imgDict:
+        idx = nameList.index(i)
+        df.loc[idx][imgDict[i][0]] = imgDict[i][1]
 
-
-
-    column = ["id","name"] + categories_list
-
-    df = DataFrame(columns=column)
-    for idx,val in enumerate(contents_list):
-        contents = [""] * (len(categories_list) + 2)
-        contents[0] = str(idx+1)
-        contents[1] = nameList[idx]
-        for v in val:
-            contents[v[0]+2] = v[1]
-
-        df = df.append(Series(contents,index=df.columns),ignore_index=True)
-        i+=1
-
+    # connection to db and input dataframe to db
     conn = sqlite3.connect("minwon.db")
     df.to_sql("minwon_info",conn,if_exists="append",index=False)
 
